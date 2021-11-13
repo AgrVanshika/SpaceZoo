746dc4f3660947b21dec60696d9226247a20cec7
Manila Agrawal <va332@njit.edu>
2021-11-13 16:33:28 -0500

    twilio code add

--- src/twilio-sms-api/main.py
+++ src/twilio-sms-api/main.py
@@ -1,1 +1,13 @@
 print('hello')
+import twilio
+import twilio.rest
+try:
+    client = twilio.rest.TwilioRestClient(account_pyp, suth_token)
+    message = client.messages.create(
+        body = "Hello world",
+        from_ = '+3456722222',
+        to = '7832104955'
+        )
+except twilio.TwilioRestException as e:
+    print(e)    
+
