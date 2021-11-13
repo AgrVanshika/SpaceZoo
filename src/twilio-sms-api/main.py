96d28ff79b5d97c7d34009fe6115bcbc59bc9811
AOrps <aeo34@njit.edu>
2021-11-13 17:00:33 -0500

    twilio send_sms code

--- src/twilio-sms-api/main.py
+++ src/twilio-sms-api/main.py
@@ -1,13 +1,32 @@
-print('hello')
-import twilio
-import twilio.rest
-try:
-    client = twilio.rest.TwilioRestClient(account_pyp, suth_token)
-    message = client.messages.create(
-        body = "Hello world",
-        from_ = '+3456722222',
-        to = '7832104955'
-        )
-except twilio.TwilioRestException as e:
-    print(e)    
+# Ensure that you have a creds.py file
+from creds import ACCOUNT, TOKEN
+
+from twilio.rest import Client
+
+client = Client(ACCOUNT, TOKEN)
+
+# func send_sms(to, from, message)
+
+message = client.messages \
+                .create(
+                     body="SpaceZoo.tech",
+                     from_='+15155178952',
+                     to='+1'
+                 )
+
+print(message.sid)
+
+# https://www.twilio.com/code-exchange/send-sms-message-twilio-phone-number
+# https://www.twilio.com/docs/sms/quickstart/python
+
+
+# try:
+#     client = twilio.rest.TwilioRestClient(account_pyp, suth_token)
+#     message = client.messages.create(
+#         body = "Hello world",
+#         from_ = '+3456722222',
+#         to = '7832104955'
+#         )
+# except twilio.TwilioRestException as e:
+#     print(e)    
 
