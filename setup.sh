d44ff0bd06a086208c59cf2efba59dfd4cf91387
AOrps <aeo34@njit.edu>
2021-11-14 03:44:01 -0500

    Space Zoo logos and new favicons

--- setup.sh
+++ setup.sh
@@ -7,4 +7,4 @@ Script meant to show what dependencies to use with Server
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo apt install nginx golang-go python3 git make -y
+sudo apt install nginx golang-go python3 git make wget curl -y
