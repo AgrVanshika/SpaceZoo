763916947ad83bc4701bbcaadb00b1a1e1807f67
AOrps <aeo34@njit.edu>
2021-11-14 08:44:21 -0500

    db and cockroachdb stuff re-organized

--- src/main.go
+++ src/main.go
@@ -4,25 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"os/exec"
-	"path/filepath"
 	"strconv"
-	"strings"
 	"text/template"
 
 	L "github.com/AOrps/spaceZoo/src/lib"
 )
 
 const PORT = 9990
-
-func getAbsLocation() string {
-	file, _ := exec.LookPath(os.Args[0])
-	path, _ := filepath.Abs(file)
-	index := strings.LastIndex(path, string(os.PathSeparator))
-	ret := path[:index]
-	return ret
-}
 
 /*
 visualization
